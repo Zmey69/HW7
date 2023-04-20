ad35c8e78b805788abaf3eb24d1af2180268391f
Zmey69 <125044870+Zmey69@users.noreply.github.com>
2023-04-20 20:10:30 +0500

    Домашняя работа на тему циклы

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        for (int i = 2; i<=100; i=i+2) {
+            System.out.println(" Уволить айтишника с id " +i);
+
+        }
     }
+
 }
